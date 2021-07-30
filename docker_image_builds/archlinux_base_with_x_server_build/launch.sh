d8c7ac09413e510bd617a0a9403ce534c6820c98
Paralock <nathanael.mercaldo@gmail.com>
2021-07-29 23:23:01 -0500

    Updated arch and ubuntu containers and added basic react client/server container

--- docker_image_builds/archlinux_base_with_x_server_build/launch.sh
+++ docker_image_builds/archlinux_base_with_x_server_build/launch.sh
@@ -1,9 +1,10 @@
-docker run --device=/dev/input/event18:/dev/input/event18         \
-           --device=/dev/input/event19:/dev/input/event19         \
+podman run --device=/dev/input/event19:/dev/input/event19         \
+           --device=/dev/input/event22:/dev/input/event22         \
            --device=/dev/nvidiactl:/dev/nvidiactl           \
            --device=/dev/nvidia-modeset:/dev/nvidia-modeset \
            --device=/dev/nvidia0:/dev/nvidia0 \
            --device=/dev/tty2:/dev/tty2         \
+           --systemd=false \
            --cap-add=SYS_TTY_CONFIG                      \
            -it \
            --rm \
